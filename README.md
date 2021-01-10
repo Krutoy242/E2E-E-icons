2aa50dca55dfa21bc873cb24e79be934a3586221
Krutoy242 <Krutoy242@gmail.com>
2021-01-10 17:19:57 +0300

    💯 Add Ability to specify item meta or mod
    For example **Basalt (ic2)**

--- README.md
+++ README.md
@@ -1,12 +1,65 @@
+# ![img](https://git.io/JLhnf) E2E-E Icons
 
-## 📦 Items of Interest
+This TypeScript program designed to automatically turn text in GitHub Markdown files into Minecraft's item icons, parsing their names or brackets.
+
+For example:
+
+| This string | Turns into this |
+|---|---|
+| Iron Ingot Anvil Triple Battery | ![Iron Ingot](https://git.io/JLjca) ![Anvil](https://git.io/JLjcu) ![Triple Battery](https://git.io/JLjcV)|
+
+
+## Other examples
+
+Note that we can mark different `mods` or `metas` for same names
+
+<table>
+<tr><td>
+<strong>Before iconification:</strong>
+</td><td>
+<strong>After iconification:</strong>
+</td></tr>
+<td>
+Amber (Biomes O' Plenty)
+Lens (AA)
+Factory Block
+Futura Block (5)
+
+----
+
+Basalt (advancedrocketry)
+Basalt (Chisel)
+Basalt (EM)
+Basalt (ic2)
+</td>
+<td>
+
+![Amber](https://git.io/JLhjR)
+![Lens](https://git.io/JLhj8)
+![Factory Block](https://git.io/JLjnM)
+![Futura Block](https://git.io/JLjsJ)
+
+-----
+
+![Basalt](https://git.io/JLjsf)
+![Basalt](https://git.io/JLjnc)
+![Basalt](https://git.io/JLjnZ)
+![Basalt](https://git.io/JLjnn)
+</td>
+</tr>
+</table>
+
+## More icons
+
+
+### 📦 Items of Interest
 
 Items that could be used in crafts:
 
 ![Oxidized Ferric Sand](https://git.io/JLhhe)![Vitrified Sand](https://git.io/JLhhs)![Diamond Lattice](https://git.io/JLhhm)![Galactic Potato](https://git.io/JLhhY)![Compressed Ender](https://git.io/JLhhI)![Charged Draconium Block](https://git.io/JLhpx)![Glowstone Nano-Particles](https://git.io/JLhpA)![Blue Quartz](https://git.io/JLhhW)![Carton](https://git.io/JLhh2)![Emeradic Crystal](https://git.io/JLhhk)![Energized Clathrate](https://git.io/JLhhf)![Destabilized Clathrate](https://git.io/JLhph)![Crushed Emerald](https://git.io/JLhhq)![Crushed End Stone](https://git.io/JLhhT)![Topaz](https://git.io/JLhj4)![HSLA Steel Ingot](https://git.io/JLhht)![Infinity Reagent](https://git.io/JLhhL)![Silicon Plate](https://git.io/JLhhn)![Steel Sheet](https://git.io/JLhhB)![Titanium Aluminide Sheet](https://git.io/JLhpN)![Iridium Rod](https://git.io/JLhh4)![Titanium Aluminide Rod](https://git.io/JLhpp)
 
 
-## 💧 Liquids of Interest
+### 💧 Liquids of Interest
 
 Liquids that should have more uses:
 
@@ -17,3 +70,4 @@ Liquids that should have more uses:
 | ![Latex](https://git.io/JLjvI) | Latex | ![Crystallized Obsidian](https://git.io/JLjvt) | Crystallized Obsidian |
 | ![Pahoehoe Lava](https://git.io/JLjvk) | Pahoehoe Lava | ![Hotspring Water](https://git.io/JLjvs) | Hotspring Water |
 | ![Menril Resin](https://git.io/JLjvL) | Menril Resin | ![Construction Foam](https://git.io/JLjv3) | Construction Foam |
+
--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
 import { Base, Tree } from './Tree'
 import gitio from 'gitio'
+import _ from 'lodash'
+import chalk from 'chalk'
 
 
 //##################################################################
@@ -16,7 +18,7 @@ import gitio from 'gitio'
 // actuallyadditions__battery_bauble__0__486c93ecf7a496392b74e28a24f1966f
 // actuallyadditions__battery_bauble__0
 
-const dot=(s='.')=>process.stdout.write(s)
+const write=(s='.')=>process.stdout.write(s)
 
 const argv = yargs(hideBin(process.argv)).argv
 
@@ -34,9 +36,27 @@ argv.filename ??= 'README.md'
 let md = fs.readFileSync(argv.filename as string, 'utf8')
 
 // Make set from CSV
-const csv: {[key:string]:string}[] = csv_parse(fs.readFileSync('items-with-nbt-csv.csv', 'utf8'), {columns: true})
-const nameSet = new Set<string>(csv.map(o=>o['Display name']))
+type TellmeEntry = {
+  "Mod name": string
+  "Registry name": string
+  "Item ID": string
+  "Meta/dmg": string
+  "Subtypes": string
+  "Display name": string
+  "Ore Dict keys": string
+  "NBT": string
+}
 
+const csv: TellmeEntry[] = csv_parse(fs.readFileSync('items-with-nbt-csv.csv', 'utf8'), {columns: true})
+const nameMap = new Map<string, TellmeEntry[]>()
+
+csv.forEach(c=>{
+  const name = c['Display name']
+  nameMap.set(name, nameMap.get(name) ?? [])
+  nameMap.get(name).push(c)
+})
+
+const map = [...nameMap].sort((a,b)=>b[1].length - a[1].length).filter(o=>o[1].length>1)
 
 
 //##################################################################
@@ -48,22 +68,94 @@ const nameSet = new Set<string>(csv.map(o=>o['Display name']))
 
 // Find this words in text file
 const replaces: {from:RegExp, base:Base, name:string}[] = []
+const unclears: string[] = []
 let namesCount = 0
-dot('Looking for Item names ')
-for (const itemName of nameSet) {
+write('Looking for Item names ')
+for (const [itemName, itemArr] of nameMap) {
   if(itemName == '') continue
-
-  const rgx = new RegExp(`(^|[^\\[\\w])(${RegexEscape(itemName)})(?!\\]|\\w)`, 'gmi')
-  const match = md.match(rgx)
-  if(match) {
-    pushForReplacing(itemName, rgx);
-  }
-  if(namesCount%1000==0) dot()
+  if(namesCount%1000==0) write()
   namesCount++
+
+  const escaped:string = RegexEscape(itemName)
+  const rgx = new RegExp(`(?<prefix>^|[^\\[\\w])(?<capture>${escaped})(?!\\]|\\w)`, 'gmi')
+
+  for (const match of md.matchAll(rgx)) {
+    handleMatch(match, itemName, itemArr, rgx, escaped)
+  }
 }
 
-function pushForReplacing(itemName: string, rgx: RegExp) {
-  const item = csv.find(o=>o['Display name']==itemName)
+function handleMatch(match: RegExpMatchArray, itemName: string, itemArr: TellmeEntry[], rgx: RegExp, escaped:string) {
+  if(itemArr.length === 1) return pushForReplacing(itemName, rgx, itemArr[0])
+
+  const sub_md = md.substring(match.index)
+  const rgx_tail = '(?<tail>\\s*\\((?<option>[^\\)]+)\\))'
+  const opts_rgx = new RegExp(`^${escaped}${rgx_tail}`, 'mi')
+  const sub_match = sub_md.match(opts_rgx)
+  if(!sub_match) {
+    pushUnclear(itemName, itemArr)
+    pushForReplacing(itemName, rgx, itemArr.pop())
+    return
+  }
+
+  const option = sub_match.groups.option
+  const glob_rgx = new RegExp(`(?<capture>${escaped}${RegexEscape(sub_match.groups.tail)})`, 'gmi')
+  const num = parseInt(option)
+  if(!isNaN(num)) return pushForReplacing(itemName, glob_rgx, itemArr.find(o=> parseInt(o['Meta/dmg']) == num))
+  
+  const modToAbbr1 = (str:string)=>str
+    .replace(/[^\w ]/, '') // Remove special chars
+    .split(' ').map(s=>s[0]).join('') // First letter of each word
+
+  const modToAbbr2 = (str:string)=>str
+    .replace(/[^A-Z0-9]/, '') // Remove special chars
+  
+  const opLow = option.toLowerCase()
+  for (const f of [
+    (o:TellmeEntry)=>o['Mod name'].toLowerCase().startsWith(opLow),
+    (o:TellmeEntry)=>modToAbbr1(o['Mod name']).toLowerCase().startsWith(opLow),
+    (o:TellmeEntry)=>modToAbbr2(o['Mod name']).toLowerCase().startsWith(opLow),
+    (o:TellmeEntry)=>o['Registry name'].split(':')[0].startsWith(opLow),
+  ]) {
+    const optedItem = itemArr.find(f)
+    if(optedItem) {
+      pushForReplacing(itemName, glob_rgx, optedItem)
+      break
+    }
+  }
+}
+
+function pushUnclear(itemName: string, itemArr: TellmeEntry[]) {
+  let s = (`${chalk.bgGreen.black(itemName)} have alts`)
+
+  function example(str:string):string {
+    return ` Example: ${chalk.green(str)}`
+  }
+
+  const uniqMods = _.uniqBy(itemArr, 'Mod name')
+  if(uniqMods.length > 1) {
+    s += (` from different mods.`+
+    ` Specify by adding `+`mod name, mod abbr. or mod id.`+
+    example(`${itemName} (${uniqMods[0]['Mod name']})`))
+  } else {
+    const metas = itemArr.map(o=>parseInt(o['Meta/dmg'])).sort()
+    const uniq_metas = _.uniq(metas)
+
+    if(metas.length != uniq_metas.length) {
+      const last = itemArr.pop()
+      s += (` with different meta AND definition.`+
+      ` Use braket handlers instead of names.`+
+      example(`<${last['Registry name']}:${last['Meta/dmg']}>`))
+    } else {
+      s += (` with different meta.`+
+      ` Add meta in brackets.`+
+      example(`${itemName} (${metas.pop()})`))
+    }
+  }
+
+  unclears.push(s)
+}
+
+function pushForReplacing(itemName: string, rgx: RegExp, item: TellmeEntry) {
   replaces.push({
     name:itemName,
     from: rgx,
@@ -76,8 +168,12 @@ replaces.sort((a, b) => b.name.length - a.name.length)
 
 
 console.log(' done')
-if(replaces.length>0) {
-  console.log('found items: ', replaces.map(o=>o.name))
+
+if(unclears.length)
+  console.log(unclears.join('\n'))
+
+if(replaces.length) {
+  console.log('found names: ', chalk.bold.yellow(replaces.length))
 } else {
   console.log('No replacables found.')
   process.exit(0)
@@ -108,7 +204,7 @@ function getSerialized(base: Base): string {
   return `${s}__${base[2]}`
 }
 
-console.log('Replacing ');
+write('Replacing ');
 
 let tmpMd = md;
 const shortReplaces: {from:RegExp, name:string, p: Promise<string>}[] = []
@@ -118,7 +214,7 @@ replaces.forEach(r=>{
 
   tmpMd = tmpMd.replace(r.from, () => {
     const p:Promise<string> = gitio(`https://github.com/Krutoy242/E2E-E-icons/raw/main/x32/${ser}.png`)
-    p.then(()=>dot())
+    p.then(()=>write())
     shortReplaces.push({p, ...r})
     return ''
   })
@@ -126,8 +222,9 @@ replaces.forEach(r=>{
 
 Promise.all(shortReplaces.map(r=>r.p)).then((shorts)=>{
   shorts.forEach((short,i)=>{
-    md = md.replace(shortReplaces[i].from, `$1![${shortReplaces[i].name}](${short})`)
+    md = md.replace(shortReplaces[i].from, `$<prefix>![${shortReplaces[i].name}](${short})`)
   })
 
   fs.writeFileSync(argv.filename as string, md)
+  console.log(' done');
 })
