d7f98b1a2e67450612a53f993087f1feb2b45c1e
Krutoy242 <Krutoy242@gmail.com>
2022-01-18 13:42:38 +0300

    🧱 Prepare to change environment

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { bracketsSearch } from './src/searcher'
 const argv = yargs(hideBin(process.argv)).argv
 
 // Argument warning
-if(!argv.filename) {
+if(!argv['filename']) {
   const userArgs = Object.entries(argv).filter(a=>a[0]!='_' && a[0]!='$0')
   console.log('Run this task with --filename="path/to/file.md" argument')
   if(userArgs.length>0) console.log('Arguments:', userArgs)
@@ -14,14 +14,14 @@ if(!argv.filename) {
 }
 
 // Temp for tests
-argv.filename ??= 'README.md'
+argv['filename'] ??= 'README.md'
 
 // Aliases
-if (argv.s) argv.silent = true
-if (argv.silent) argv.s = true
+if (argv['s']) argv['silent'] = true
+if (argv['silent']) argv['s'] = true
 
 bracketsSearch(
   argv as any,
-  fs.readFileSync(argv.filename as string, 'utf8'),
-  replaced=>fs.writeFileSync(argv.filename as string, replaced)
+  fs.readFileSync(argv['filename'] as string, 'utf8'),
+  replaced=>fs.writeFileSync(argv['filename'] as string, replaced)
 )
